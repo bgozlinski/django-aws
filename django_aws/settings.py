4c9c1fbb347a1a88f4126a6f1fec8e1ccd78c56a
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-02-15 04:45:59 +0100

    Correcting path to media files

--- django_aws/settings.py
+++ django_aws/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/images/'
-MEDIA_ROOT = [BASE_DIR / 'static/images']
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 
 
